c5175827d2a68e0305c0b2163ce438d3bdedcb68
pau <jethro.riosa@gmail.com>
2022-05-18 22:46:39 +0800

    investigate

--- lib/coingecko_messenger_web/controllers/page_controller.ex
+++ lib/coingecko_messenger_web/controllers/page_controller.ex
@@ -1,12 +1,14 @@
 defmodule CoingeckoMessengerWeb.PageController do
   use CoingeckoMessengerWeb, :controller
 
+  require Logger
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def webhook(conn, params) do
-    raise params
+    Logger.info(params)
     render(conn, "index.html")
   end
 end
